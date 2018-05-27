812a0186d650271f5837f94bf89d3de11ac911c0
ZapilinEntertainment <mahariusls@yandex.ru>
2018-05-27 20:21:43 +0500

    resourceCost backup

--- ResourcesCost.cs
+++ ResourcesCost.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public static class ResourcesCost {
 
-	public static ResourceContainer[] GetCost (int id, byte level) {
+	public static ResourceContainer[] GetCost (int id) {
 		ResourceContainer[] cost = new ResourceContainer[0];
 		switch ( id ) {
 		case Structure.MINE_ID:
 			cost = new ResourceContainer[] {
-				new ResourceContainer(ResourceType.metal_M, 4 * level), new ResourceContainer(ResourceType.metal_K, 10 * level), new ResourceContainer(ResourceType.Plastics, 4 * level)
+				new ResourceContainer(ResourceType.metal_M, 4), new ResourceContainer(ResourceType.metal_K, 10), new ResourceContainer(ResourceType.Plastics, 4)
 			} ;
 			break;
-		case Structure.WIND_GENERATOR_ID:
+		case Structure.WIND_GENERATOR_1_ID:
 			cost = new ResourceContainer[]{
 				new ResourceContainer(ResourceType.metal_K, 12), new ResourceContainer(ResourceType.metal_M, 4), new ResourceContainer(ResourceType.metal_E, 2)
 			} ;
 			break;
-		case Structure.ORE_ENRICHER_ID:
+		case Structure.ORE_ENRICHER_2_ID:
 			cost = new ResourceContainer[]{
 				new ResourceContainer(ResourceType.Concrete, 60), new ResourceContainer(ResourceType.metal_K, 20), new ResourceContainer(ResourceType.metal_M, 25)
 			};
@@ -34,7 +34,7 @@ public static class ResourcesCost {
 				new ResourceContainer(ResourceType.metal_N, 10), new ResourceContainer(ResourceType.Plastics, 15)
 			} ;
 			break;
-		case Structure.FUEL_FACILITY_ID:
+		case Structure.FUEL_FACILITY_3_ID:
 			cost  = new ResourceContainer[]{
 				new ResourceContainer(ResourceType.metal_K, 25), new ResourceContainer(ResourceType.metal_M, 15), new ResourceContainer(ResourceType.Concrete, 60)
 			};
@@ -63,205 +63,185 @@ public static class ResourcesCost {
 				new ResourceContainer(ResourceType.Concrete, 100)
 			} ;
 			break;		
-		case Structure.STORAGE_ID:
-			switch ( level ) {
-			case 1:
+		case Structure.STORAGE_1_ID:
 				cost = new ResourceContainer[]{
 					new ResourceContainer(ResourceType.Concrete, 10), new ResourceContainer(ResourceType.metal_K, 4), new ResourceContainer(ResourceType.metal_M, 1),
 					new ResourceContainer(ResourceType.Plastics, 10)
 				};
 				break;
-			case 2:
+		case Structure.STORAGE_2_ID:
 				cost = new ResourceContainer[]{
 					new ResourceContainer(ResourceType.Concrete, 20), new ResourceContainer(ResourceType.metal_K, 10), new ResourceContainer(ResourceType.metal_M, 2),
 					new ResourceContainer(ResourceType.Plastics, 25)
 				};
 				break;
-			case 3:
+		case Structure.STORAGE_3_ID:
 				cost = new ResourceContainer[]{
 					new ResourceContainer(ResourceType.Concrete, 25), new ResourceContainer(ResourceType.metal_K, 6), new ResourceContainer(ResourceType.metal_M, 4),
 					new ResourceContainer(ResourceType.Plastics, 40)
 				};
 				break;
-			case 5:
+		case Structure.STORAGE_5_ID:
 				cost = new ResourceContainer[]{
 					new ResourceContainer(ResourceType.Plastics, 400), new ResourceContainer(ResourceType.metal_K, 50), new ResourceContainer(ResourceType.metal_E, 12),
 					new ResourceContainer(ResourceType.Concrete, 270), new ResourceContainer(ResourceType.metal_M, 15)
 				} ;
 				break;
-			}
-			break;
-		case Structure.HOUSE_ID:
-			switch ( level ) {
-			case 1: 	
+		case Structure.HOUSE_1_ID: 	
 				cost = new ResourceContainer[]{
 					new ResourceContainer(ResourceType.metal_K, 2), new ResourceContainer(ResourceType.Plastics, 4), new ResourceContainer(ResourceType.Lumber, 20)
 				};
 				break;
-			case 2:
+		case Structure.HOUSE_2_ID:
 				cost = new ResourceContainer[]{
 					new ResourceContainer(ResourceType.Concrete, 60), new ResourceContainer(ResourceType.metal_K, 10), new ResourceContainer(ResourceType.metal_E, 2),
 					new ResourceContainer(ResourceType.Plastics, 50)
 				};
 				break;
-			case 3:
+		case Structure.HOUSE_3_ID:
 				cost = new ResourceContainer[]{
 					new ResourceContainer(ResourceType.Concrete, 60), new ResourceContainer(ResourceType.metal_K, 10), new ResourceContainer(ResourceType.metal_E, 8),
 					new ResourceContainer(ResourceType.Plastics, 100)
 				};
 				break;
-			case 5:
+		case Structure.HOUSE_5_ID:
 				cost = new ResourceContainer[]{
 					new ResourceContainer(ResourceType.Plastics, 420), new ResourceContainer(ResourceType.metal_K, 25), new ResourceContainer(ResourceType.metal_E, 12),
 					new ResourceContainer(ResourceType.Concrete, 250), new ResourceContainer(ResourceType.metal_M, 10)
 				} ;
 				break;
-			}
-			break;
-		case Structure.ENERGY_CAPACITOR_ID:
-			switch (level) {
-			case 1: 
+		case Structure.ENERGY_CAPACITOR_1_ID: 
 				cost = new ResourceContainer[] {
 					new ResourceContainer(ResourceType.metal_K, 2), new ResourceContainer(ResourceType.metal_E, 4), new ResourceContainer(ResourceType.metal_N, 2)
 				};
 				break;
-			case 2:
+		case Structure.ENERGY_CAPACITOR_2_ID:
 				cost = new ResourceContainer[] {
 					new ResourceContainer(ResourceType.metal_K, 10), new ResourceContainer(ResourceType.metal_E, 12), new ResourceContainer(ResourceType.metal_N, 4),
 				} ;
 				break;
-			case 3:
+		case Structure.ENERGY_CAPACITOR_3_ID:
 				cost = new ResourceContainer[] {
 					new ResourceContainer(ResourceType.metal_K, 12), new ResourceContainer(ResourceType.metal_E, 12), new ResourceContainer(ResourceType.metal_N, 5),
 				} ;
 				break;
-			}
-			break;
-		case Structure.FARM_ID:
-			switch (level) {
-			case 1:
+		case Structure.FARM_1_ID:
 				cost = new ResourceContainer[]{
 					new ResourceContainer(ResourceType.Lumber, 50), new ResourceContainer(ResourceType.metal_K, 4), new ResourceContainer(ResourceType.metal_M, 2)
 				} ;
 				break;
-			case 2:
+		case Structure.FARM_2_ID:
 				cost = new ResourceContainer[]{
 					new ResourceContainer(ResourceType.Plastics, 20), new ResourceContainer(ResourceType.metal_K, 10), new ResourceContainer(ResourceType.metal_M, 6)
 				} ;
 				break;
-			case 3:
+		case Structure.FARM_3_ID:
 				cost = new ResourceContainer[]{
 					new ResourceContainer(ResourceType.Plastics, 20), new ResourceContainer(ResourceType.metal_K, 13), new ResourceContainer(ResourceType.metal_M, 20)
 				} ;
 				break;
-			case 4:
+		case Structure.FARM_4_ID:
 				cost = new ResourceContainer[]{
 					new ResourceContainer(ResourceType.Plastics, 80), new ResourceContainer(ResourceType.metal_K, 12), new ResourceContainer(ResourceType.metal_E, 10)
 				} ;
 				break;
-			case 5:
+		case Structure.FARM_5_ID:
 				cost = new ResourceContainer[]{
 					new ResourceContainer(ResourceType.Plastics, 400), new ResourceContainer(ResourceType.metal_K, 50), new ResourceContainer(ResourceType.metal_E, 20),
 					new ResourceContainer(ResourceType.Concrete, 250), new ResourceContainer(ResourceType.metal_M, 25)
 				} ;
 				break;
-			}
-			break;
-		case Structure.LUMBERMILL_ID:
-			switch (level) {
-			case 1:
+		case Structure.LUMBERMILL_1_ID:
 				cost = new ResourceContainer[]{
 					new ResourceContainer(ResourceType.Lumber, 30), new ResourceContainer(ResourceType.metal_K, 5), new ResourceContainer(ResourceType.metal_M, 10)
 				} ;
 				break;
-			case 2:
+		case Structure.LUMBERMILL_2_ID:
 				cost = new ResourceContainer[]{
 					new ResourceContainer(ResourceType.Plastics, 20), new ResourceContainer(ResourceType.metal_K, 8), new ResourceContainer(ResourceType.metal_M, 6)
 				}  ;
 				break;
-			case 3:
+		case Structure.LUMBERMILL_3_ID:
 				cost = new ResourceContainer[]{
 					new ResourceContainer(ResourceType.Plastics, 20), new ResourceContainer(ResourceType.metal_K, 10), new ResourceContainer(ResourceType.metal_M, 8)
 				} ;
 				break;
-			case 4:
+		case Structure.LUMBERMILL_4_ID:
 				cost = new ResourceContainer[]{
 					new ResourceContainer(ResourceType.Plastics, 80), new ResourceContainer(ResourceType.metal_K, 16), new ResourceContainer(ResourceType.metal_E, 8)
 				} ;
 				break;
-			case 5:
+		case Structure.LUMBERMILL_5_ID:
 				cost = new ResourceContainer[]{
 					new ResourceContainer(ResourceType.Plastics, 400), new ResourceContainer(ResourceType.metal_K, 50), new ResourceContainer(ResourceType.metal_E, 16),
 					new ResourceContainer(ResourceType.Concrete, 250), new ResourceContainer(ResourceType.metal_M, 28)
 				} ;
 				break;
-			}
-			break;
-		case Structure.PLASTICS_FACTORY_ID:
+		case Structure.PLASTICS_FACTORY_4_ID:
 			cost = new ResourceContainer[]{
 				new ResourceContainer(ResourceType.Plastics, 20), new ResourceContainer(ResourceType.metal_K, 25), new ResourceContainer(ResourceType.metal_M, 20),
 				new ResourceContainer(ResourceType.Concrete, 50)
 			} ;
 			break;
-		case Structure.FOOD_FACTORY_ID:
-			switch (level) {
-			case 4:
+		case Structure.FOOD_FACTORY_4_ID:
 				cost = new ResourceContainer[]{
 					new ResourceContainer(ResourceType.Plastics, 40), new ResourceContainer(ResourceType.metal_K, 10), new ResourceContainer(ResourceType.metal_M, 10),
 					new ResourceContainer(ResourceType.Concrete, 50), new ResourceContainer(ResourceType.metal_E, 10)
 				} ;
 				break;
-			case 5:
+		case Structure.FOOD_FACTORY_5_ID:
 				cost = new ResourceContainer[]{
 					new ResourceContainer(ResourceType.Plastics, 400), new ResourceContainer(ResourceType.metal_K, 50), new ResourceContainer(ResourceType.metal_E, 20),
 					new ResourceContainer(ResourceType.Concrete, 250), new ResourceContainer(ResourceType.metal_M, 30)
 				} ;
 				break;
-			}
-			break;
-		case Structure.SMELTERY_ID:
-			switch (level) {
-				case 1: cost = new ResourceContainer[] {
+		case Structure.SMELTERY_1_ID: cost = new ResourceContainer[] {
 					new ResourceContainer(ResourceType.metal_K, 10), new ResourceContainer(ResourceType.metal_M, 10), new ResourceContainer(ResourceType.Lumber, 50)
 				} ;
 				break;
-			case 2:
+		case Structure.SMELTERY_2_ID:
 				cost = new ResourceContainer[] {
 					new ResourceContainer(ResourceType.metal_K, 10), new ResourceContainer(ResourceType.metal_M, 8), new ResourceContainer(ResourceType.Plastics, 25),
 					new ResourceContainer(ResourceType.Concrete, 40)
 				} ;
 				break;
-			case 3:
+		case Structure.SMELTERY_3_ID:
 				cost = new ResourceContainer[] {
 					new ResourceContainer(ResourceType.metal_K, 12), new ResourceContainer(ResourceType.metal_M, 10), new ResourceContainer(ResourceType.Plastics, 50),
 					new ResourceContainer(ResourceType.metal_E, 20)
 				} ;
 				break;
-			case 5:
+		case Structure.SMELTERY_5_ID:
 				cost = new ResourceContainer[]{
 					new ResourceContainer(ResourceType.Plastics, 350), new ResourceContainer(ResourceType.metal_K, 50), new ResourceContainer(ResourceType.metal_E, 18),
 					new ResourceContainer(ResourceType.Concrete, 250), new ResourceContainer(ResourceType.metal_M, 40)
 				} ;
 				break;
-			}
-			break;
-		case Structure.HQ_ID:
-			switch (level) {
-			case 2:
+		case Structure.HQ_2_ID:
 				cost = new ResourceContainer[]{
 					new ResourceContainer(ResourceType.Concrete, 100), new ResourceContainer(ResourceType.metal_K, 12), new ResourceContainer(ResourceType.metal_E, 6),
 					new ResourceContainer(ResourceType.Plastics, 45), new ResourceContainer(ResourceType.metal_N, 4)
 				};
 				break;
-			}
+		case Structure.HQ_3_ID:
+			cost = new ResourceContainer[]{
+				new ResourceContainer(ResourceType.Concrete, 80), new ResourceContainer(ResourceType.metal_K, 8), new ResourceContainer(ResourceType.metal_E, 12),
+				new ResourceContainer(ResourceType.Plastics, 60), new ResourceContainer(ResourceType.metal_N, 8)
+			};
 			break;
-		case Structure.BIOGENERATOR_ID:
+		case Structure.HQ_4_ID:
+			cost = new ResourceContainer[]{
+				new ResourceContainer(ResourceType.Concrete, 200), new ResourceContainer(ResourceType.metal_K, 30), new ResourceContainer(ResourceType.metal_E, 20),
+				new ResourceContainer(ResourceType.Plastics, 250), new ResourceContainer(ResourceType.metal_N, 20)
+			};
+			break;
+		case Structure.BIOGENERATOR_2_ID:
 			cost = new ResourceContainer[] {
 				new ResourceContainer(ResourceType.Concrete, 60), new ResourceContainer(ResourceType.metal_K, 12), new ResourceContainer(ResourceType.metal_M, 6),
 				new ResourceContainer(ResourceType.metal_E, 4)
 			};
 			break;
-		case Structure.HOSPITAL_ID:
+		case Structure.HOSPITAL_2_ID:
 			cost = new ResourceContainer[]{
 				new ResourceContainer(ResourceType.Concrete, 120), new ResourceContainer(ResourceType.metal_K, 15), new ResourceContainer(ResourceType.metal_E, 5)
 			};
@@ -271,13 +251,13 @@ public static class ResourcesCost {
 				new ResourceContainer(ResourceType.metal_K, 15), new ResourceContainer(ResourceType.metal_M, 5), new ResourceContainer(ResourceType.metal_N, 12)
 			};
 			break;
-		case Structure.GRPH_REACTOR_ID:
+		case Structure.GRPH_REACTOR_4_ID:
 			cost = new ResourceContainer[]{
 				new ResourceContainer(ResourceType.metal_K, 30), new ResourceContainer(ResourceType.metal_M, 40), new ResourceContainer(ResourceType.metal_N, 30),
 				new ResourceContainer(ResourceType.Concrete, 120), new ResourceContainer(ResourceType.Plastics, 100)
 			};
 			break;
-		case Structure.MINERAL_POWERPLANT_ID:
+		case Structure.MINERAL_POWERPLANT_2_ID:
 			cost = new ResourceContainer[]{
 				new ResourceContainer(ResourceType.Concrete, 100), new ResourceContainer(ResourceType.metal_K, 12), new ResourceContainer(ResourceType.metal_M, 20),
 				new ResourceContainer(ResourceType.metal_E, 10)
